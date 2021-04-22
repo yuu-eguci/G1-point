793517bad0c5d4ea2c03358a36ff10d4c612c448
yuu-eguci <yuu.eguci@gmail.com>
2021-04-22 20:38:01 +0900

    Update flask_line_receiver.py

--- flask_line_receiver.py
+++ flask_line_receiver.py
@@ -1,4 +1,4 @@
-"""line_operator
+"""flask_line_receiver
 
 LINE とのやりとりを行うスクリプトです。
 """
@@ -31,6 +31,7 @@ handler = WebhookHandler(consts.LINE_CHANNEL_SECRET)
 # 必須ではないけれど、サーバに上がったことを確認するためにトップページを追加しておきます。
 @app.route('/')
 def top_page():
+    # NOTE: logger でも Heroku ログにちゃんと出る。
     logger.debug('Here is root page.')
     return 'Here is root page.'
 
@@ -39,11 +40,6 @@ def top_page():
 @app.route('/callback', methods=['POST'])
 def callback_post():
 
-    # ここで行うことは……
-    # - Push message を行うための送信先を取得。たぶん group id...??
-    # - 誰の発言? を取得。(名前と user_id) user id は要らないか。
-    # - メッセージの内容を取得。
-
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -51,8 +47,32 @@ def callback_post():
     body = request.get_data(as_text=True)
     logger.debug('Request body: ' + body)
 
+    # NOTE: body の内容はこんな感じ。
+    #       dict として扱い、値を取り出すこともできます。
+    #       ただし handler によって add した関数内で利用するのが正道な気がする。
+    # {
+    #     "destination": "Uf2485b0560fd4931794caf4e4dab033d",
+    #     "events": [
+    #         {
+    #             "type": "message",
+    #             "message": {
+    #                 "type": "text", "id": "13930066619344", "text": "HELLO"
+    #             },
+    #             "timestamp": 1619087553397,
+    #             "source": {
+    #                 "type": "group", "groupId": "C19709b8f8...acd9f538",
+    #                 "userId": "U226ec6476abd.....e94a3fa9d3be56"
+    #             },
+    #             "replyToken": "f5bf4ee22dd54....4489279adb0",
+    #             "mode": "active"
+    #         }
+    #     ]
+    # }
+
     # handle webhook body
     try:
+        # NOTE: ドキュメント https://github.com/line/line-bot-sdk-python#webhookhandler
+        #       handler は、別途 @handler.add を定義することで利用する。
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
@@ -60,12 +80,45 @@ def callback_post():
     return 'OK'
 
 
+# NOTE: @handler.add した関数は、handler.handle 関数によって呼び出される、
+#       ってイメージで多分 OK.
 @handler.add(MessageEvent, message=TextMessage)
-def reply_message(event):
-    # reply のテスト。
+def on_get_message(event):
+
+    # ここで行うことは……
+    # - Push message を行うための送信先を取得。たぶん group id...??
+    # - 誰の発言? を取得。(名前と user_id) user id は要らないか。
+    # - メッセージの内容を取得。
+
+    # Push message を行うための送信先。
+    group_id = event.source.group_id
+    print(dict(group_id=group_id))
+
+    # 発言者の id。
+    user_id = event.source.userId
+    # 発言者の情報。
+    # NOTE: ドキュメント https://github.com/line/line-bot-sdk-python#get_profileself-user_id-timeoutnone
+    user_profile = line_bot_api.get_profile(user_id)
+    print(dict(
+        display_name=user_profile.display_name,
+        user_id=user_profile.user_id,
+        picture_url=user_profile.picture_url,
+        status_message=user_profile.status_message,
+    ))
+
+    # 返答するための token。
+    # NOTE: line_bot_api.reply_message(reply_token, TextSendMessage(text='...')) と使用。
+    reply_token = event.reply_token
+    print(dict(reply_token=reply_token))
+
+    # メッセージの内容。
+    message_text = event.message.text
+    print(dict(message_text=message_text))
+
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='Send from line_bot_api.reply_message, you sent...: ' + event.message.text))
+        reply_token,
+        TextSendMessage(text=f'Send from line_bot_api.reply_message, you sent...: {message_text}')
+    )
 
 
 if __name__ == '__main__':
